507e54e9a3289b0e2134fc2b35502bf5da250073
Rohan Phanse <58747665+rohanphanse@users.noreply.github.com>
2021-06-21 00:24:34 -0700

    Add prism.js code highlighting

--- components/Metadata.js
+++ components/Metadata.js
@@ -11,6 +11,8 @@ const Metadata = props => {
                 <link rel = "icon" href = "/images/logo.png" />
                 <title>{props.title ? `${props.title} | Rohan Phanse` : "Rohan Phanse"}</title>
                 <link href = "https://use.fontawesome.com/releases/v5.13.0/css/all.css" rel = "stylesheet" crossOrigin = "anonymous"></link>
+                <link href="/prism/prism.css" rel="stylesheet" />
+                <script src="/prism/prism.js"></script>
             </Head>
         </>
     )
